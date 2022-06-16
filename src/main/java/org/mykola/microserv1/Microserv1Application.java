07e79c5bb8b22d36cb4cc750f409a85d64717118
DeliriumTraemens <oskinnick@gmail.com>
2022-06-16 12:38:40 +0300

    Initial commit

---
+++ src/main/java/org/mykola/microserv1/Microserv1Application.java
@@ -0,0 +1,13 @@
+package org.mykola.microserv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microserv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microserv1Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/mykola/microserv1/Microserv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mykola.microserv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Microserv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
